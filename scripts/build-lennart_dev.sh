5fee85e38afba6c57b0e73cb0e7ce25c710ff862
Ram Nanduri <Ram.Nanduri@skane.se>
2024-01-16 12:24:09 +0100

    build edit

--- scripts/build-lennart_dev.sh
+++ scripts/build-lennart_dev.sh
@@ -6,7 +6,7 @@ SCRIPT_NAME='' #For Dev use only
 
 
 # Build CDM docker image and start on lennart
-version="0.0.2_dev"
+version="0.0.3_dev"
 # version=$(python -c "import cll_genie_app.__version__; print(__version__.__version__)")
 
 #!/bin/bash
@@ -26,29 +26,30 @@ docker build --no-cache --network host --target cll_genie_app_dev -t cll_genie:$
 
 set -o allexport; source .env; set +o allexport
 
-docker run \
-       -e CLARITY_USER=$CLARITY_USER \
-       -e CLARITY_PASSWORD=$CLARITY_PASSWORD \
-       -e CLARITY_HOST=$CLARITY_HOST \
-       -e DB_HOST=$DB_HOST \
-       -e DB_PORT=$DB_PORT \
-       -e FLASK_DEBUG=1 \
-       -e SCRIPT_NAME=$SCRIPT_NAME \
-       -e LOG_LEVEL="DEBUG" \
-       -p 5814:5000 \
-       --dns "10.212.226.10" \
-       --name cll_genie_app_dev_2 \
-       -v /data/lymphotrack/cll_results/:/cll_genie/results/ \
-       -v /data/lymphotrack/results/lymphotrack_dx/:/data/lymphotrack/results/lymphotrack_dx/ \
-       -v /data/lymphotrack/logs:/cll_genie/logs \
-       -d \
-       "cll_genie:$version"
+# docker run \
+#        -e CLARITY_USER=$CLARITY_USER \
+#        -e CLARITY_PASSWORD=$CLARITY_PASSWORD \
+#        -e CLARITY_HOST=$CLARITY_HOST \
+#        -e DB_HOST=$DB_HOST \
+#        -e DB_PORT=$DB_PORT \
+#        -e FLASK_DEBUG=1 \
+#        -e SCRIPT_NAME=$SCRIPT_NAME \
+#        -e LOG_LEVEL="DEBUG" \
+#        -p 5814:5000 \
+#        --dns "10.212.226.10" \
+#        --name cll_genie_app_dev_3 \
+#        -v /data/lymphotrack/cll_results/:/cll_genie/results/ \
+#        -v /data/lymphotrack/cll_results_dev/:/cll_genie/results_dev/ \
+#        -v /data/lymphotrack/results/lymphotrack_dx/:/data/lymphotrack/results/lymphotrack_dx/ \
+#        -v /data/lymphotrack/logs:/cll_genie/logs \
+#        -d \
+#        "cll_genie:$version"
 
 
 #docker run -e DB_HOST='172.17.0.1' -e CLARITY_USER=$CLARITY_USER -e CLARITY_PASSWORD=$CLARITY_PASSWORD -e CLARITY_HOST=$CLARITY_HOST -e DB_PORT=$DB_PORT -e FLASK_DEBUG=1 -e SCRIPT_NAME=$SCRIPT_NAME -e LOG_LEVEL="DEBUG" -p 5813:5000 --name cll_genie_app_dev -v /data/lymphotrack/cll_results/:/cll_genie/results/ -v /data/lymphotrack/results/lymphotrack_dx/:/data/lymphotrack/results/lymphotrack_dx/ -v /data/lymphotrack/logs:/cll_genie/logs "cll_genie:$version"
 
 
-#docker run -e CLARITY_USER=$CLARITY_USER -e CLARITY_PASSWORD=$CLARITY_PASSWORD -e CLARITY_HOST=$CLARITY_HOST -e DB_HOST=$DB_HOST -e DB_PORT=$DB_PORT -e FLASK_DEBUG=1 -e SCRIPT_NAME=$SCRIPT_NAME -e LOG_LEVEL="DEBUG" -p 5814:5000 --dns "10.212.226.10" --name cll_genie_app_dev_2 -v /data/lymphotrack/cll_results/:/cll_genie/results/ -v /data/lymphotrack/results/lymphotrack_dx/:/data/lymphotrack/results/lymphotrack_dx/ -v /data/lymphotrack/logs:/cll_genie/logs -d "cll_genie:$version"
+#docker run -e CLARITY_USER=$CLARITY_USER -e CLARITY_PASSWORD=$CLARITY_PASSWORD -e CLARITY_HOST=$CLARITY_HOST -e DB_HOST=$DB_HOST -e DB_PORT=$DB_PORT -e FLASK_DEBUG=1 -e SCRIPT_NAME=$SCRIPT_NAME -e LOG_LEVEL="DEBUG" -p 5814:5000 --dns "10.212.226.10" --name cll_genie_app_dev_2 -v /data/lymphotrack/cll_results/:/cll_genie/results/ -v /data/lymphotrack/cll_results_dev/:/cll_genie/results_dev/ -v /data/lymphotrack/results/lymphotrack_dx/:/data/lymphotrack/results/lymphotrack_dx/ -v /data/lymphotrack/logs:/cll_genie/logs -d "cll_genie:$version"
 
 
 #docker run --user www-data \
